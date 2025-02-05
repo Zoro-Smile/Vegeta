144a151f51b2e74fc2016841fbf166d748c92285
Zoro-Smile <nagaprakashudhaya2022@gmail.com>
2025-02-05 10:01:47 +0530

    js calcullator program is updated..

--- coding.js
+++ coding.js
@@ -1,1 +1,23 @@
-console.log("js is important for mern stack");
+function appendNumber(number) {
+    const screen = document.getElementById('screen');
+    screen.value += number;
+}
+
+function appendOperator(operator) {
+    const screen = document.getElementById('screen');
+    screen.value += ' ' + operator + ' ';
+}
+
+function clearScreen() {
+    const screen = document.getElementById('screen');
+    screen.value = '';
+}
+
+function calculate() {
+    const screen = document.getElementById('screen');
+    try {
+        screen.value = eval(screen.value);
+    } catch (error) {
+        screen.value = 'Error';
+    }
+}
